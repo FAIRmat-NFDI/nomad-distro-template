3241feb1d5040b63bea8982ab0534cf522504667
Ahmed Ilyas <ahmed.n.ilyas@gmail.com>
2025-03-18 15:58:37 +0100

    Refetch token when response is 404 (#15)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,9 @@
-from nomad.config import config
 import pytest
+from nomad.config import config
 
 
 @pytest.fixture(scope="session")
 def auth():
     from nomad.client import Auth
 
-    return Auth(
-        user=config.client.user,
-        password=config.client.password,
-        from_api=True,
-    )
+    return Auth(user=config.client.user, password=config.client.password, from_api=True)
