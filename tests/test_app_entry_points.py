9ba3043dc1ecc324e226dddd13359efbb14e34dd
Haoyu (Daniel) YANG 杨浩宇 <yanghaoyu97@outlook.com>
2025-10-09 11:56:14 +0200

    remove unused apps fixture (#131)

--- tests/test_app_entry_points.py
+++ tests/test_app_entry_points.py
@@ -7,19 +7,9 @@ if TYPE_CHECKING:
     from nomad.client import Auth
 
 
-@pytest.fixture(scope="module")
-def apps(auth: "Auth"):
-    """The list of app entrypoints."""
-    resp = make_request_with_retry(get_request, "apps/entry-points", auth)
-    payload = resp.json()
-    apps = payload.get("data", [])
-    assert apps, "No apps returned by /apps/entry-points"
-    return apps
-
-
 @pytest.mark.parametrize(
     "app",
-    [pytest.param(a, id=a.get("path", "<no-path>")) for a in make_request_with_retry(get_request, "apps/entry-points", auth=None).json().get("data", [])],
+    [pytest.param(app, id=app.get("path", "<no-path>")) for app in make_request_with_retry(get_request, "apps/entry-points", auth=None).json().get("data", [])],
 )
 def test_app_entry_point(auth: "Auth", app):
     """Each app entry point should respond successfully."""
